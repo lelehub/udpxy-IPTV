ff62b4f04be2d222fc11e77bf99d98be3944571c
JiaLe <38939982+lelehub@users.noreply.github.com>
2025-05-16 16:50:48 +0800

    Update record.sh

--- record.sh
+++ record.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 git pull
 date_str=$(date '+%Y-%m-%d %H:%M')
-echo "${date_str}: Record it" > ./README.md
+echo "${date_str}: Record it" > ./CHANGELOG.md
 
-git add ./README.md
+git add ./CHANGELOG.md
 git commit -m "${date_str}"
